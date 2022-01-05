7f5909cd3de6d0fea217ef73cb7937ea5b40cf8f
Saman Malakjan <saman.malakjan@student.manchester.ac.uk>
2022-01-05 16:19:46 +0000

    Deciding turns implemented, Board made efficient the first state of the game which is deciding turns is implemented and board only uses 3 draw rects compared to 6 before, since the background is drawn and then playable rectangles.

--- text.py
+++ text.py
@@ -4,30 +4,40 @@ from constants import *
 
 class Text:
     pygame.init()
-    font = pygame.font.SysFont("Arial", 15)
+    font = pygame.font.SysFont("Arial", 17)
     
-    def __init__(self, text):
+    def __init__(self, text, x, y, env_width, env_height):
         self.content = text
+        self.render_content()
+        self.x_cord, self.y_cord = x, y
+        self.original_x_cord, self.original_y_cord = x, y
+        self.env_width, self.env_height  = env_width, env_height
+
+    def render_content(self):
         self.rendered_content = Text.font.render(str(self.content), True, BLACK)
         self.width = self.rendered_content.get_width()
         self.height = self.rendered_content.get_height()
-        self.x_cord, self.y_cord = 0, 0
 
-    def calc_paddings(self, env_width, env_height):
-        width_padding = (env_width - self.width) / 2
-        height_padding = (env_height - self.height) / 2 
+    def set_content(self, text):
+        self.content = text
+        self.render_content()
+
+    def calc_paddings(self):
+        width_padding = (self.env_width - self.width) / 2
+        height_padding = (self.env_height - self.height) / 2 
 
         return width_padding, height_padding
 
-    def add_paddings(self, x, y, env_width, env_height):
-        width_padding, height_padding = self.calc_paddings(env_width, env_height)
-        self.x_cord = x + width_padding
-        self.y_cord = y + height_padding
-
+    def add_paddings(self):
+        width_padding, height_padding = self.calc_paddings()
+        x_cord = self.x_cord + width_padding
+        y_cord = self.y_cord + height_padding
+        return x_cord, y_cord 
 
     def collide_with_mouse(self, x_mouse, y_mouse):
         return self.x_cord < x_mouse < self.x_cord + self.width \
                and self.y_cord < y_mouse < self.height
 
     def draw(self, surface):
-        surface.blit(self.rendered_content, (self.x_cord, self.y_cord))
+        x_cord, y_cord = self.add_paddings()
+        surface.blit(self.rendered_content, (x_cord, y_cord))
